d546578aef0b0d9d7118b2f284799b20d9a929ca
MAKSytka <44722008+MAKSytka@users.noreply.github.com>
2026-07-30 23:30:13 +0200

    chore(kty): route accepted runtime to v18

--- src/kty_station_sim/setup.py
+++ src/kty_station_sim/setup.py
@@ -41,14 +41,15 @@ setup(
             "mechatronics_cycle = kty_station_sim.mechatronics_cycle:main",
             "mechatronics_cycle_v2 = kty_station_sim.mechatronics_cycle_v2:main",
             # Preserve historical validator markers while the accepted executable
-            # is routed to runtime v17:
+            # is routed to runtime v18:
             # mechatronics_cycle_v3 = kty_station_sim.mechatronics_cycle_v11:main
             # mechatronics_cycle_v3 = kty_station_sim.mechatronics_cycle_v12:main
             # mechatronics_cycle_v3 = kty_station_sim.mechatronics_cycle_v13:main
             # mechatronics_cycle_v3 = kty_station_sim.mechatronics_cycle_v14:main
             # mechatronics_cycle_v3 = kty_station_sim.mechatronics_cycle_v15:main
             # mechatronics_cycle_v3 = kty_station_sim.mechatronics_cycle_v16:main
-            "mechatronics_cycle_v3 = kty_station_sim.mechatronics_cycle_v17:main",
+            # mechatronics_cycle_v3 = kty_station_sim.mechatronics_cycle_v17:main
+            "mechatronics_cycle_v3 = kty_station_sim.mechatronics_cycle_v18:main",
             "mechatronics_cycle_v10 = kty_station_sim.mechatronics_cycle_v10:main",
             "mechatronics_cycle_v11 = kty_station_sim.mechatronics_cycle_v11:main",
             "mechatronics_cycle_v12 = kty_station_sim.mechatronics_cycle_v12:main",
@@ -57,6 +58,7 @@ setup(
             "mechatronics_cycle_v15 = kty_station_sim.mechatronics_cycle_v15:main",
             "mechatronics_cycle_v16 = kty_station_sim.mechatronics_cycle_v16:main",
             "mechatronics_cycle_v17 = kty_station_sim.mechatronics_cycle_v17:main",
+            "mechatronics_cycle_v18 = kty_station_sim.mechatronics_cycle_v18:main",
             "fill_estimator = kty_station_sim.fill_estimator:main",
             "fill_estimator_v2 = kty_station_sim.fill_estimator_v2:main",
             "depth_perception = kty_station_sim.depth_perception:main",
